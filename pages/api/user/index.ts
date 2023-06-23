5dc7b7fd5fbfa0bde0fae6c419c29e6b120cd159
nkruger <nicolas.nkruger@gmail.com>
2023-06-23 15:04:39 -0300

    feat: :sparkles: post

--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -1,12 +1,34 @@
-import { MyRoute, generateRoutes } from "@/utils/routes";
+import { generateRoutes } from "@/utils/routes";
 import { NextApiHandler } from "next";
 import { readFileConverter } from "@/utils/readFileConverter";
+import { writeFileConverter } from "@/utils/writeFileConverter";
 
 type User = {
   name: string;
   email: string;
   password: string;
   id: string;
+};
+
+const keysOfUser: (keyof User)[] = ["email", "id", "name", "password"];
+
+const post: NextApiHandler = (req, res) => {
+  const users: User[] = readFileConverter("./db/user.json");
+  const body: Omit<User, "id"> = req.body;
+
+  const newUser: User = {
+    ...body,
+    id: `${Math.random()}`.slice(2, 5),
+  };
+
+  const invalidUser = keysOfUser.some((key) => !newUser[key as keyof User]);
+
+  if (invalidUser) return res.status(400).json({ msg: "incomplete user" });
+
+  writeFileConverter("./db/user.json", [...users, newUser]);
+  res.status(201).json({
+    msg: "created",
+  });
 };
 
 const get: NextApiHandler = (req, res) => {
@@ -19,6 +41,7 @@ const get: NextApiHandler = (req, res) => {
 const apiHandler: NextApiHandler = (req, res) => {
   const route = generateRoutes({
     GET: get,
+    POST: post,
   });
 
   return route(req, res);
