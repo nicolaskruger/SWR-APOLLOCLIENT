24aec21ffabe62cdf222b22bb777b1932b6ca129
nkruger <nicolas.nkruger@gmail.com>
2023-06-26 17:30:56 -0300

    test: :test_tube: path user

--- pages/api/user/index.ts
+++ pages/api/user/index.ts
@@ -85,12 +85,38 @@ const deleteFunc: NextApiHandler = (req, res) => {
   return res.json({ msg: "user delete with success" });
 };
 
+const patch: NextApiHandler = (req, res) => {
+  const user: Partial<User> = req.body;
+
+  const users: User[] = readFileConverter("./db/user.json");
+
+  const foundUser = users.find((u) => user.id === u.id);
+
+  if (!foundUser) return res.status(404).json({ msg: "user not found" });
+
+  writeFileConverter(
+    "./db/user.json",
+    users.map((u) => {
+      if (u.id === user.id) {
+        return {
+          ...u,
+          ...user,
+        };
+      }
+      return u;
+    })
+  );
+
+  return res.json({ msg: "update with success" });
+};
+
 const apiHandler: NextApiHandler = (req, res) => {
   const route = generateRoutes({
     GET: get,
     POST: post,
     PUT: put,
     DELETE: deleteFunc,
+    PATCH: patch,
   });
 
   return route(req, res);
