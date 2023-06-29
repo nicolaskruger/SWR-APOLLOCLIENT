54af2bfcfeed2e6f415532f8acf2ce242c2c1d08
nkruger <nicolas.nkruger@gmail.com>
2023-06-29 15:29:51 -0300

    test: :test_tube: useUser test falling

---
+++ hooks/user/useUser.spec.ts
@@ -0,0 +1,31 @@
+import useSwr, { SWRResponse } from "swr";
+
+const myUser = {
+  email: "user@email",
+  id: "user",
+  name: "user",
+  password: "123",
+};
+
+jest.mock("swr");
+describe("useUser", () => {
+  it("should convert items correctly", () => {
+    jest.mocked(useSwr).mockImplementationOnce(() => {
+      const data: User = myUser;
+
+      const response: SWRResponse = {
+        data,
+        error: false,
+        isLoading: false,
+      } as SWRResponse;
+
+      return response;
+    });
+  });
+
+  const { isError, isLoading, user } = useUser("user");
+
+  expect(isError).toBe(false);
+  expect(isLoading).toBe(false);
+  expect(user).toStrictEqual(myUser);
+});
---
+++ hooks/user/useUser.ts
@@ -0,0 +1,9 @@
+type UseUserReturn = {
+  user: User;
+  isLoading: boolean;
+  isError: boolean;
+};
+
+const useUser = (id: string): UseUserReturn => {
+  throw new Error("not implemented");
+};
