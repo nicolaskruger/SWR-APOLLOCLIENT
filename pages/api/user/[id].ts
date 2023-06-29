ae26fa93a6967dc377c857871c29668743283fcf
nkruger <nicolas.nkruger@gmail.com>
2023-06-29 15:14:03 -0300

    test: :white_check_mark: find user by id

--- pages/api/user/[id].ts
+++ pages/api/user/[id].ts
@@ -1,8 +1,26 @@
 import { NextApiHandler } from "next";
 import { generateRoutes } from "../../utils/routes";
+import { readFileConverter } from "../../utils/readFileConverter";
+
+type User = {
+  name: string;
+  id: string;
+  email: string;
+  password: string;
+};
 
 const get: NextApiHandler = (req, res) => {
-  throw new Error();
+  const { id } = req.query;
+
+  const users = readFileConverter<User[]>("./db/user.json");
+
+  const user = users.find((user) => id === user.id);
+
+  if (!user) return res.status(404).json({ msg: "user not found" });
+
+  return res.status(200).json({
+    ...user,
+  });
 };
 
 const apiHandler: NextApiHandler = (req, res) => {
