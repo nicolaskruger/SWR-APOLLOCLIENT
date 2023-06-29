05d13060c0f955095e620e536234cdaa11ad8c64
nkruger <nicolas.nkruger@gmail.com>
2023-06-29 14:42:06 -0300

    test: :white_check_mark: user page

--- components/user-page/user-page.spec.tsx
+++ components/user-page/user-page.spec.tsx
@@ -1,10 +1,8 @@
 import { render, screen } from "@testing-library/react";
-import axios from "axios";
 import { UserPage } from "./user-page";
-import { cache } from "swr/_internal";
-import { SWRConfig } from "swr";
+import useSwr, { SWRResponse } from "swr";
 
-jest.mock("axios");
+jest.mock("swr");
 
 const delay = (ms: number) => new Promise((res) => setTimeout(res, ms));
 
@@ -13,49 +11,40 @@ describe("<UserPage/>", () => {
     jest.clearAllMocks();
   });
   it("should render an error msg when error", async () => {
-    jest.mocked(axios).get.mockImplementation(() => {
-      throw new Error();
+    jest.mocked(useSwr).mockImplementationOnce(() => {
+      return { error: true } as SWRResponse;
     });
-    render(
-      <SWRConfig value={{ dedupingInterval: 0 }}>
-        <UserPage />
-      </SWRConfig>
-    );
+    render(<UserPage />);
     await delay(1000);
     const p = screen.getByTestId<HTMLParagraphElement>("p-error-message");
     expect(p.textContent).toBe("can't fetch");
   });
   it("should render an loading mdg when loading", () => {
-    jest.mocked(axios).get.mockImplementation(async () => {
-      await delay(3000);
+    jest.mocked(useSwr).mockImplementationOnce(() => {
+      return { isLoading: true } as SWRResponse;
     });
 
-    render(
-      <SWRConfig value={{ dedupingInterval: 0 }}>
-        <UserPage />
-      </SWRConfig>
-    );
+    render(<UserPage />);
 
     const p = screen.getByTestId<HTMLParagraphElement>("p-loading");
 
     expect(p.textContent).toBe("loading...");
   });
   it("should render a component fetch is correct", async () => {
-    jest.mocked(axios).get.mockResolvedValue({
-      data: [
-        {
-          name: "string",
-          email: "string",
-          id: "string",
-          password: "string",
-        },
-      ],
+    jest.mocked(useSwr).mockImplementationOnce(() => {
+      return {
+        data: [
+          {
+            name: "string",
+            email: "string",
+            id: "string",
+            password: "string",
+          },
+        ],
+      } as SWRResponse;
     });
-    render(
-      <SWRConfig value={{ dedupingInterval: 0 }}>
-        <UserPage />
-      </SWRConfig>
-    );
+
+    render(<UserPage />);
 
     await delay(1000);
 
