08e8832f5f3f6c783bfa84878026494ce8fe7da1
nkruger <nicolas.nkruger@gmail.com>
2023-06-29 16:02:49 -0300

    test: :test_tube: UserByIdPage failing test

---
+++ src/app/user/[id]/page.spec.tsx
@@ -0,0 +1,50 @@
+import { render, screen } from "@testing-library/react";
+import { useUser } from "../../../../hooks/user/useUser";
+import UserById from "./page";
+
+jest.mock("../../../../hooks/user/useUser");
+
+const renderPage = (info: ReturnType<typeof useUser>) => {
+  jest.mocked(useUser).mockReturnValueOnce({
+    ...info,
+  });
+  render(<UserById params={{ id: "nicolas" }} />);
+};
+
+describe("<UserById/>", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  it("should render an error msg when error", () => {
+    renderPage({
+      isError: true,
+      isLoading: false,
+      user: {} as User,
+    });
+
+    expect(screen.queryByTestId("p-error")).toBeInTheDocument();
+  });
+  it("should render an loading msg when loading", () => {
+    renderPage({
+      isError: false,
+      isLoading: true,
+      user: {} as User,
+    });
+
+    expect(screen.queryByTestId("p-loading")).toBeInTheDocument();
+  });
+  it("should loading correctly", () => {
+    renderPage({
+      isError: false,
+      isLoading: false,
+      user: {
+        email: "nicolas",
+        id: "nicolas",
+        name: "nicolas",
+        password: "123",
+      } as User,
+    });
+
+    expect(screen.queryByTestId("div-user")).toBeInTheDocument();
+  });
+});
