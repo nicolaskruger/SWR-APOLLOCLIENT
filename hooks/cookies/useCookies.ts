d0a13844e7ed4044d4cfe9c28476d54366256933
nkruger <nicolas.nkruger@gmail.com>
2023-07-04 16:03:12 -0300

    test: :white_check_mark:

--- hooks/cookies/useCookies.ts
+++ hooks/cookies/useCookies.ts
@@ -1,7 +1,15 @@
 import { useState } from "react";
+import { getMyCookies, setMyCookies } from "./helper";
 
 const useCookies = (key: string): [string, (value: string) => void] => {
-  return useState("");
+  const [cookies, setCookies] = useState(getMyCookies(key));
+
+  const set = (value: string) => {
+    setMyCookies(key, value);
+    setCookies(value);
+  };
+
+  return [cookies, set];
 };
 
 export { useCookies };
