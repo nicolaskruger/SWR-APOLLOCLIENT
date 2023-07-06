49c2eeb78870dff2553175a04ea51191b9f4b4b6
nkruger <nicolas.nkruger@gmail.com>
2023-07-06 17:09:18 -0300

    feat: :sparkles: me api

--- pages/api/swr/me/index.ts
+++ pages/api/swr/me/index.ts
@@ -1,7 +1,41 @@
-import { NextApiHandler } from "next";
+import { NextApiHandler, NextApiResponse } from "next";
+import { generateRoutes } from "../../../utils/routes";
+import jwt from "jsonwebtoken";
+import { readFileConverter } from "../../../utils/readFileConverter";
+
+const returnError = (res: NextApiResponse) =>
+  res.status(401).json({ msg: "Unauthorized" });
+
+const get: NextApiHandler = (req, res) => {
+  const auth = req?.headers?.authorization;
+
+  if (!auth) return returnError(res);
+
+  try {
+    const decode = jwt.verify(auth.replace(/Bearer */, ""), "shh") as {
+      id: string;
+    };
+
+    if (!decode?.id) return returnError(res);
+
+    const users: User[] = readFileConverter("./db/user.json");
+
+    const user = users.find((user) => user.id === decode.id);
+
+    if (!user) return returnError(res);
+
+    return res.json(user);
+  } catch {
+    return returnError(res);
+  }
+};
 
 const me: NextApiHandler = (req, res) => {
-  return res.status(501);
+  const routes = generateRoutes({
+    GET: get,
+  });
+
+  return routes(req, res);
 };
 
 export default me;
