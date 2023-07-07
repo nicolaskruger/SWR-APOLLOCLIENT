88a58f0f77d35894c7705f6dd44ec523a1e4618e
nkruger <nicolas.nkruger@gmail.com>
2023-07-07 15:47:40 -0300

    feat: :sparkles: useMe

---
+++ hooks/me/useMe.ts
@@ -0,0 +1,21 @@
+import useSwr from "swr";
+import { useToken } from "../token/useToken";
+import axios, { AxiosError } from "axios";
+
+const useMe = () => {
+  const [token] = useToken();
+  const props = useSwr<User, AxiosError<{ msg: string }>, [string, string]>(
+    ["/swr/me", token],
+    async ([url, token]) => {
+      const response = await axios.get<User>(url, {
+        headers: {
+          Authorization: token,
+        },
+      });
+      return response.data;
+    }
+  );
+  return props;
+};
+
+export { useMe };
