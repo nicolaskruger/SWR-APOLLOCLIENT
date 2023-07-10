ba03faad23b707404878748d7088416b88991c6c
nkruger <nicolas.nkruger@gmail.com>
2023-07-10 17:13:54 -0300

    test: :white_check_mark: me repository

--- pages/utils/me/me-repository.ts
+++ pages/utils/me/me-repository.ts
@@ -1,6 +1,30 @@
+import jwt from "jsonwebtoken";
+import { readFileConverter } from "../readFileConverter";
+
+const throwError = () => {
+  throw new Error("invalid token");
+};
+
 const meRepository = {
-  decodeToken: (token: string): User => {
-    return {} as User;
+  decodeToken: (auth: string): User => {
+    if (!auth) throwError();
+    try {
+      const decode = jwt.verify(auth.replace(/Bearer */, ""), "shh") as {
+        id: string;
+      };
+
+      if (!decode?.id) return throwError();
+
+      const users: User[] = readFileConverter("./db/user.json");
+
+      const user = users.find((user) => user.id === decode.id);
+
+      if (!user) return throwError();
+
+      return user;
+    } catch {
+      return throwError();
+    }
   },
 };
 
