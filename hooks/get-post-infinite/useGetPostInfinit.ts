277607acaf583666adbe96f4654e702e914b965d
nkruger <nicolas.nkruger@gmail.com>
2023-07-13 16:09:12 -0300

    test: :test_tube: useGetPostInfinit

--- hooks/get-post-infinite/useGetPostInfinit.ts
+++ hooks/get-post-infinite/useGetPostInfinit.ts
@@ -2,6 +2,8 @@ import axios from "axios";
 import useSWRInfinite from "swr/infinite";
 import { useToken } from "../token/useToken";
 
+const PAGE_SIZE = 3;
+
 const useGetPostInfinite = () => {
   const [token] = useToken();
   const props = useSWRInfinite(
@@ -21,7 +23,29 @@ const useGetPostInfinite = () => {
     }
   );
 
-  return props;
+  const concatData = props.data
+    ? props.data.reduce((acc, curr) => [...acc, ...curr], [])
+    : [];
+  const isLoadingMore =
+    props.isLoading ||
+    (props.size > 0 &&
+      props.data &&
+      typeof props.data[props.size - 1] === "undefined");
+  const isEmpty = props.data?.[0]?.length === 0;
+  const isReachingEnd =
+    isEmpty ||
+    (props.data && props.data[props.data.length - 1]?.length < PAGE_SIZE);
+  const isRefreshing =
+    props.isValidating && props.data && props.data.length === props.size;
+
+  return {
+    ...props,
+    concatData,
+    isLoadingMore,
+    isEmpty,
+    isReachingEnd,
+    isRefreshing,
+  };
 };
 
 export { useGetPostInfinite };
