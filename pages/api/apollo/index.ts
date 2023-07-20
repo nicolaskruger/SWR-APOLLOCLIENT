ea06ad2a6ff8b47cd2d64b71bfb9dc9d1cd66e2c
nkruger <nicolas.nkruger@gmail.com>
2023-07-20 14:39:52 -0300

    feat: :sparkles: me route

--- pages/api/apollo/index.ts
+++ pages/api/apollo/index.ts
@@ -2,6 +2,7 @@ import { ApolloServer } from "@apollo/server";
 import { startServerAndCreateNextHandler } from "@as-integrations/next";
 import { gql } from "graphql-tag";
 import { loginRepository } from "../../utils/login/login-repository";
+import { meRepository } from "../../utils/me/me-repository";
 
 const resolvers = {
   Query: {
@@ -9,15 +10,25 @@ const resolvers = {
       value: string,
       { email, password }: { email: string; password: string }
     ) => loginRepository.login(email, password),
+    me: (_: any, { token }: { token: string }) =>
+      meRepository.decodeToken(token),
   },
 };
 
 const typeDefs = gql`
+  type User {
+    name: String
+    email: String
+    password: String
+    id: String
+    url: String!
+  }
   type Token {
     token: String
   }
   type Query {
     login(email: String, password: String): Token
+    me(token: String): User
   }
 `;
 
